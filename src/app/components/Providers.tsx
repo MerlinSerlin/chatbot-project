620b230e7a7009e074e722fda015e18ee29fab7c
Marshall Serlin <marshallserlin@gmail.com>
2023-10-17 09:38:14 -0700

    wired up chatgpt

---
+++ src/app/components/Providers.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React, { FC, ReactNode } from 'react'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+interface ProvidersProps {
+  children: ReactNode
+}
+
+const Providers: FC<ProvidersProps> = ({children}) => {
+    const queryClient = new QueryClient()
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
+
+export default Providers;
