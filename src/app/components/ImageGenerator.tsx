d0cd9a5fdd8d3071c34956908e298338ddca1b07
Marshall Serlin <marshallserlin@gmail.com>
2023-10-22 12:44:13 -0700

    fix for chevrondown icon

--- src/app/components/ImageGenerator.tsx
+++ src/app/components/ImageGenerator.tsx
@@ -84,7 +84,8 @@ const ImageGenerator: FC<ImageGenerator> = ({}) => {
       className={
         cn(
           overlaidBtnClassName,
-          'text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
+          // 'text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
+          "bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
         )
       }
       onClick={() => {
